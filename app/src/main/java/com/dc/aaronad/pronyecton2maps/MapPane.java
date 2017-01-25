c56d94c4c784e3458c9df35f41a659b06edc5a01
AaronADRC88 <layun36@gmail.com>
2017-01-25 20:00:01 +0100

    boton ubicacion y permisos

--- app/src/main/java/com/dc/aaronad/pronyecton2maps/MapPane.java
+++ app/src/main/java/com/dc/aaronad/pronyecton2maps/MapPane.java
@@ -1,14 +1,25 @@
 package com.dc.aaronad.pronyecton2maps;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapPane extends AppCompatActivity implements OnMapReadyCallback {
-
+    private static final int LOCATION_REQUEST_CODE = 1;
+    private GoogleMap gMap;
     private FirstMapFragment mFirstMapFragment;
 
     @Override
@@ -27,6 +38,66 @@ public class MapPane extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        gMap = googleMap;
+        LatLng vigo = new LatLng(42.2154941, -8.8120584);
+        gMap.addMarker(new MarkerOptions()
+                .position(vigo)
+                .title("FESTA RACHADA!!!"));
+        CameraPosition cameraPosition = CameraPosition.builder()
+                .target(vigo)
+                .zoom(10)
+                .build();
+
+        gMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        // Controles UI
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            gMap.setMyLocationEnabled(true);
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Mostrar diálogo explicativo
+            } else {
+                // Solicitar permiso
+                ActivityCompat.requestPermissions(
+                        this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        LOCATION_REQUEST_CODE);
+            }
+        }
+
+        gMap.getUiSettings().setZoomControlsEnabled(true);
+
+        // Marcadores
+        gMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)));
+
 
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == LOCATION_REQUEST_CODE) {
+            // ¿Permisos asignados?
+            if (permissions.length > 0 &&
+                    permissions[0].equals(Manifest.permission.ACCESS_FINE_LOCATION) &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                gMap.setMyLocationEnabled(true);
+            } else {
+                Toast.makeText(this, "Error de permisos", Toast.LENGTH_LONG).show();
+            }
+
+        }
+    }
 }
+
